db6a49d140992700a3974e7169426d068935c620
cxim <cxumhik@mail.ru>
2020-09-18 16:59:56 +0300

    todo new rools for adm\moders\users

--- src/main/java/com/example/demo/controler/MainController.java
+++ src/main/java/com/example/demo/controler/MainController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controler;
 
 import com.example.demo.domain.Message;
+import com.example.demo.domain.User;
 import com.example.demo.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,36 +19,44 @@ public class MainController {
 	@Autowired
 	private MessageRepo messageRepo;
 
-	@GetMapping("/")
+	@GetMapping
 	public String greetingPage(Map<String, Object> model) {
 		return "greeting";
 	}
 
 	@GetMapping("/main")
-	public String mainPage(Map<String,Object> model) {
+	public String mainPage(@RequestParam(required = false) String filter, Model model) {
 		Iterable<Message> messages = messageRepo.findAll();
-		model.put("messages", messages);
+
+		if (filter != null && !filter.isEmpty()) {
+			messages = messageRepo.findByTag(filter);
+		} else {
+			messages = messageRepo.findAll();
+		}
+
+		model.addAttribute("messages", messages);
+		model.addAttribute("filter", filter);
 		return "main";
 	}
 
 	@PostMapping("/main")
-	public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-		Message message = new Message(text, tag);
+	public String add(
+			@AuthenticationPrincipal User user,
+			@RequestParam String text,
+			@RequestParam String tag,
+			Map<String, Object> model) {
+		Message message = new Message(user, text, tag);
 		messageRepo.save(message);
 		Iterable<Message> messages = messageRepo.findAll();
 		model.put("messages", messages);
 		return "main";
 	}
 
-	@PostMapping("filter")
-	public String filter(@RequestParam String filter, Map<String, Object> model) {
-		Iterable<Message> messages;
-		if (filter != null && !filter.isEmpty()) {
-			messages = messageRepo.findByTag(filter);
-		} else {
-			messages = messageRepo.findAll();
-		}
-		model.put("messages", messages);
-		return "main";
-	}
+//	@PostMapping("filter")
+//	public String filter(@RequestParam String filter, Map<String, Object> model) {
+//		Iterable<Message> messages;
+//
+//		model.put("messages", messages);
+//		return "main";
+//	}
 }
--- src/main/java/com/example/demo/domain/Message.java
+++ src/main/java/com/example/demo/domain/Message.java
@@ -1,9 +1,6 @@
 package com.example.demo.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -14,12 +11,29 @@ public class Message {
 	private String text;
 	private String tag;
 
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private User author;
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
 	public Message() {
 	}
 
-	public Message(String text, String tag) {
+	public Message(User author, String text, String tag) {
+		this.author = author;
 		this.text = text;
 		this.tag = tag;
+	}
+
+	public String getAuthorName() {
+		return author != null ? author.getUsername() : "<none>";
 	}
 
 	public Integer getId() {
