ce8bf64d31f4788ccba2bd09624bf33c674525b9
cxim <cxumhik@mail.ru>
2020-09-20 13:42:23 +0300

    adm panel work

--- src/main/java/com/example/demo/controler/UserController.java
+++ src/main/java/com/example/demo/controler/UserController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controler;
 
+import com.example.demo.domain.Role;
+import com.example.demo.domain.User;
 import com.example.demo.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -17,5 +23,33 @@ public class UserController {
 	public String userList(Model model) {
 		model.addAttribute("users", userRepo.findAll());
 		return "userList";
+	}
+
+	@GetMapping("{user}")
+	public String goToAdm(@PathVariable User user, Model model) {
+		model.addAttribute("user", user);
+		model.addAttribute("roles", Role.values());
+		return "userGoToAdm";
+	}
+
+	@PostMapping
+	public String userSave(@RequestParam("userId") User user,
+						   @RequestParam Map<String, String> form,
+						   @RequestParam String username) {
+		user.setUsername(username);
+		Set<String> roles = Arrays.stream(Role.values())
+				.map(Role::name)
+				.collect(Collectors.toSet());
+
+		user.getRoles().clear();
+
+		for (String key : form.keySet()) {
+			if (roles.contains(key)) {
+				user.getRoles().add(Role.valueOf(key));
+			}
+		}
+
+		userRepo.save(user);
+		return "redirect:/user";
 	}
 }
